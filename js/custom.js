7dba9c8155cfcbe4e35bee651f9dd5e4b577119e
udaysarkarud <udaysarkarud@gmail.com>
2021-08-21 03:21:01 +0600

    promo code added

--- js/custom.js
+++ js/custom.js
@@ -54,7 +54,8 @@ document.getElementById('promo-btn').addEventListener('click', function () {
     const promoInput = document.getElementById('promo-input')
     if (promoInput.value == 'stevekaku') {
         const grandTotal = document.getElementById('grand-total')
-        grandTotal.innerText = parseInt(grandTotal.innerText) - (parseInt(grandTotal.innerText) / 20)
+        const promoAmount = (parseInt(grandTotal.innerText) * 20) / 100
+        grandTotal.innerText = parseInt(grandTotal.innerText) - promoAmount
     }
     promoInput.value = ''
 
